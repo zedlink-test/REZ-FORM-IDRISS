99ed1cb7697e16b0326beeff9f97cbaa5eb1775a
ZEDLINK-TEST <redzedpc@gmail.com>
2025-10-25 16:43:21 +0100

    Add comment for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Detect if we're building for GitHub Pages
 const isGithubPages = process.env.DEPLOY_ENV === 'github';
 
 export default defineConfig({
-  base: isGithubPages ? '/REZ-FORM-IDRISS/' : '/',
+  base: isGithubPages ? '/REZ-FORM-IDRISS/' : '/', // '/' for Netlify
   plugins: [react()],
 });
