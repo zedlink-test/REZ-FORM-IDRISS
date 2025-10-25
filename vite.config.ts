2151dd6baacf74221d25a989e65e6180881473b0
ZEDLINK-TEST <redzedpc@gmail.com>
2025-10-25 16:35:02 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // remove '/REZ-FORM-IDRISS/', just use '/'
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
+
