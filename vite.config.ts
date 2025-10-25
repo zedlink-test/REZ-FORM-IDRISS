a3ad950a6d8b8dcda7f33cd95aa3d05f7f21074e
ZEDLINK-TEST <redzedpc@gmail.com>
2025-10-25 16:45:40 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// Detect if we're building for GitHub Pages
-const isGithubPages = process.env.DEPLOY_ENV === 'github';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: isGithubPages ? '/REZ-FORM-IDRISS/' : '/', // '/' for Netlify
+  base: '/', // 👈 IMPORTANT for Netlify (root domain)
   plugins: [react()],
-});
+})
